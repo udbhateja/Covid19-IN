2a5dfd61ab5ddf7a8cd9b463d1baaeb1067567f0
Uday Bhateja <udbhateja@gmail.com>
2020-06-18 13:38:56 +0530

    Initial Commit

---
+++ Covid19-IN/Covid19_IN.h
@@ -0,0 +1,19 @@
+//
+//  Covid19_IN.h
+//  Covid19-IN
+//
+//  Created by JBhateja on 17/06/20.
+//  Copyright © 2020 UdayBhateja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Covid19_IN.
+FOUNDATION_EXPORT double Covid19_INVersionNumber;
+
+//! Project version string for Covid19_IN.
+FOUNDATION_EXPORT const unsigned char Covid19_INVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Covid19_IN/PublicHeader.h>
+
+
---
+++ Covid19-INExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19-INExamples
+//
+//  Created by JBhateja on 17/06/20.
+//  Copyright © 2020 UdayBhateja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
